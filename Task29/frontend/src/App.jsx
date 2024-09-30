026ec3b364ef267b6690075850636480c36f5956
Shreyas <shreyastungar762@gmail.com>
2024-09-30 22:05:36 +0530

    bug fix

--- Task29/frontend/src/App.jsx
+++ Task29/frontend/src/App.jsx
@@ -4,26 +4,28 @@ import axios from "axios";
 const App = () => {
   const [file, setFile] = useState();
   const [text, setText] = useState();
+console.log("text", text);
 
   useEffect(() => {
     axios
-      .get("http://localhost:8080/text")
+      .get("https://video-stream-backend-six.vercel.app/text")
       .then((response) => {
         setText(response.data);
+        console.log(response);
       })
       .catch((error) => console.error("Error fetching text:", error));
   }, []);
   return (
     <div className="w-full min-h-screen bg-zinc-800 flex  p-7">
       <div className="p-3 flex flex-col gap-4 w-1/2">
-        <button
+        {/* <button
           onClick={() => setFile("video")}
           className={
             "w-full p-3 text-xl font-bold bg-purple-500 hover:bg-purple-600 rounded-md text-white"
           }
         >
           Lightning_effect_with_HTML&CSS
-        </button>
+        </button> */}
         <button
           onClick={() => setFile("text")}
           className="w-full p-3 text-xl font-bold bg-purple-500 hover:bg-purple-600 rounded-md text-white"
@@ -34,7 +36,10 @@ const App = () => {
       <div className="w-1/2 flex justify-center bg-zinc-900 rounded-xl p-1">
         {file === "video" ? (
           <video controls className="w-full h-auto">
-            <source src="http://localhost:8080/video" type="video/mp4" />
+            <source
+              src="https://video-stream-backend-six.vercel.app/video"
+              type="video/mp4"
+            />
           </video>
         ) : (
           <div>
